95a151059bbe9955ab181ab61b54a80e555359b5
Gian Carlo Val Ebao <gianebao@gmail.com>
2011-01-01 19:22:02 +0800

    Standardized configuration for phpcs.bat (installation can also be found at http://pear.php.net/manual/en/package.php.php-codesniffer.php)

--- scripts/phpcs.bat
+++ scripts/phpcs.bat
@@ -13,5 +13,4 @@ REM @license   http://matrix.squiz.net/developer/tools/php_cs/licence BSD Licenc
 REM @version   CVS: $Id: phpcs.bat,v 1.3 2007-11-04 22:02:16 squiz Exp $
 REM @link      http://pear.php.net/package/PHP_CodeSniffer
 
-REM "C:\Documents\xampp\php\php.exe" -d auto_append_file="C:\Documents\xampp\htdocs\" -d auto_prepend_file="" -d include_path="C:\Documents\xampp\php\" "C:\DOCUME~2\xampp\htdocs\PHP-CO~1\scripts\phpcs.exe" %*
-"C:\Documents\xampp\php\php.exe" -d auto_append_file="C:\Documents\xampp\htdocs\" -d auto_prepend_file="" "C:\Documents\xampp\htdocs\php-codesniffer\scripts\phpcs" %*
+"PATH\TO\php.exe" "\COMPLETE\PATH\TO\php-codesniffer\scripts\phpcs" %*
